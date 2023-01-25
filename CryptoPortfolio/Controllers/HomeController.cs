4660a9086db38044ea44fa3f06835c81857cbea0
Charles Devane <devane.charles98@gmail.com>
2023-01-25 14:26:09 -0500

    Add project files.

---
+++ CryptoPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using CryptoPortfolio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace CryptoPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        string baseURL = "https://api.coingecko.com/api/v3/";
+        HttpClient client = new HttpClient();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IList<CoinEntity> coin = new List<CoinEntity>();
+            var results = await GetCoinMarketList("usd");
+            coin = JsonConvert.DeserializeObject<List<CoinEntity>>(results);
+
+            ViewData.Model = coin;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // GET : https://api.coingecko.com/api/v3/coins/markets?vs_currrency={currency}
+        // returns results as string
+        public async Task<string> GetCoinMarketList(string currency)
+        {
+            try
+            {
+                client.BaseAddress = new Uri(baseURL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await client.GetAsync("coins/markets?vs_currency=" + currency);
+             
+                return getData.Content.ReadAsStringAsync().Result;
+            } catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+            
+
+        }
+    }
+}
---
+++ CryptoPortfolio/Models/CoinEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoPortfolio.Models
+{
+    public class CoinEntity
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public float Current_Price { get; set; }
+        public float ATH { get; set; }
+    }
+}
